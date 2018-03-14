3ec3e01b90d7b7c9bb31f15908e32e8a66e37e6d
Adrian C <adc9978@rit.edu>
2018-03-14 13:36:41 -0400

    Update running_services.py

--- running_services.py
+++ running_services.py
@@ -1,14 +1,22 @@
 import subprocess
 
-command = 'service --status-all'
+def main():
+	command = 'service --status-all'
 
-command = subprocess.Popen([command],stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True)
-cmdOutput = thing.stdout.read()
-cmdList = output.split('\n')
+	command = subprocess.Popen([command],stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True)
+	cmdOutput = command.stdout.read()
+	cmdList = cmdOutput.split('\n')
 
-counter = 0
-for x in range(len(cmdList)):
-	if '+' in final[x]:
-		counter += 1
+	counter = 0
+	listServices = []
+	for x in range(len(cmdList)):
+		if '+' in cmdList[x]:
+			counter += 1
+			tempSplit = cmdList[x].split()
+			listServices.append(tempSplit[3])
 
-print("There are " + str(counter) + " services running")
+	print("There are " + str(counter) + " services running:")
+	for i in range(len(listServices)):
+		print(listServices[i])
+
+main()
