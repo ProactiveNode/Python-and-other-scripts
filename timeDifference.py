446ccd48257c56766cd9c7fac040f4e9c869860d
Adrian C <adc9978@rit.edu>
2018-04-11 16:41:22 -0400

    Update timeDifference.py

--- timeDifference.py
+++ timeDifference.py
@@ -1,1 +1,10 @@
-
+def main():
+  insertFile = raw_input("Insert file: ")
+  insertTime = raw_input("Enter time: ")
+  userGuess = {}
+  with open(insertFile) as timeFile:
+	for line in timeFile:
+                lineSplit = line.split()
+		            userGuess[lineSplit[0]] = lineSplit[1]
+        
+main()
